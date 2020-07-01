e68602b3583bb373b8213bdc58c64f0f1b75870c
Lukas Krumholz <lukas.krumholz@gmx.de>
2020-07-01 12:58:21 +0200

    Include PDFModel

--- src/tags/object/index.js
+++ src/tags/object/index.js
@@ -3,5 +3,6 @@ import { AudioPlusModel } from "./AudioPlus";
 import { HyperTextModel } from "./HyperText";
 import { ImageModel } from "./Image";
 import { TextModel } from "./Text";
+import { PDFModel } from "./PDF";
 
-export { AudioModel, AudioPlusModel, HyperTextModel, ImageModel, TextModel };
+export { AudioModel, AudioPlusModel, HyperTextModel, ImageModel, TextModel, PDFModel };
